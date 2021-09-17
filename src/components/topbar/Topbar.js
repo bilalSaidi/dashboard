10fafebe43d818d5fe50346537031f2634114c11
bilalsaidi <bilal.saidibatna@gmail.com>
2021-09-17 19:46:47 +0100

    added topbar section

---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Badge from "@material-ui/core/Badge";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+import "./topbar.css";
+const Tobpar = () => {
+  return (
+    <div className="Tobpar">
+      <div className="Tobbarwrapper">
+        <div className="rightTopbar">
+          <span className="logo">admin dashboard</span>
+        </div>
+        <div className="leftTopbar">
+          <div className="listicons">
+            <div className="icon">
+              <Badge badgeContent={4} color="error">
+                <NotificationsNone />
+              </Badge>
+            </div>
+            <div className="icon">
+              <Language />
+            </div>
+            <div className="icon">
+              <Settings />
+            </div>
+          </div>
+          <div className="imageAdmin">
+            <img
+              src={require("../../assets/profile/peronal_photo.jpg").default}
+              alt="admin panel personal photo"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tobpar;
